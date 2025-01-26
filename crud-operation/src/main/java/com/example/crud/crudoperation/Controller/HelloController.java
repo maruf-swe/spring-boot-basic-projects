fa21d6c6734e768b1a9d0eeaf42343ab9d72110c
Maruf <marufur1847@gmail.com>
2025-01-26 23:58:44 +0600

    something new

--- crud-operation/src/main/java/com/example/crud/crudoperation/Controller/HelloController.java
+++ crud-operation/src/main/java/com/example/crud/crudoperation/Controller/HelloController.java
@@ -1,10 +1,12 @@
 package com.example.crud.crudoperation.Controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String helloMaruf() {
         return "Hi Maruf";
     }
