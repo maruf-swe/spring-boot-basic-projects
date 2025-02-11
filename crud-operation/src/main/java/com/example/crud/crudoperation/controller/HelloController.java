6ca246bfc574118bbd9c0d1ca9c8328e9ff3a0cd
Maruf <marufur1847@gmail.com>
2025-02-11 23:59:57 +0600

    department entity

--- crud-operation/src/main/java/com/example/crud/crudoperation/controller/HelloController.java
+++ crud-operation/src/main/java/com/example/crud/crudoperation/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.crud.crudoperation.Controller;
+package com.example.crud.crudoperation.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
