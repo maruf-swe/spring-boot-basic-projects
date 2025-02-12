6df2ba5140ec6c2195b995721f16eda9d96ec2b0
Maruf <marufur1847@gmail.com>
2025-02-12 23:58:15 +0600

    save dep controller

--- crud-operation/src/main/java/com/example/crud/crudoperation/controller/DepartmentController.java
+++ crud-operation/src/main/java/com/example/crud/crudoperation/controller/DepartmentController.java
@@ -1,8 +1,21 @@
 package com.example.crud.crudoperation.controller;
 
+import com.example.crud.crudoperation.Service.DepartmentService;
+import com.example.crud.crudoperation.entity.Department;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DepartmentController {
+    
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping
+    public Department saveDepartment(@RequestBody Department department) {
+
+    }
 
 }
