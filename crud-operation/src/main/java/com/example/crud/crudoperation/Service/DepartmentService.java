2b8c511ac29f964dbc48ea15847d2648c3c25ca6
Maruf <marufur1847@gmail.com>
2025-02-13 00:00:12 +0600

    save dep controller

--- crud-operation/src/main/java/com/example/crud/crudoperation/Service/DepartmentService.java
+++ crud-operation/src/main/java/com/example/crud/crudoperation/Service/DepartmentService.java
@@ -1,4 +1,7 @@
 package com.example.crud.crudoperation.Service;
 
+import com.example.crud.crudoperation.entity.Department;
+
 public interface DepartmentService {
+    Department saveDepartment(Department department);
 }
--- crud-operation/src/main/java/com/example/crud/crudoperation/Service/DepartmentServiceImp.java
+++ crud-operation/src/main/java/com/example/crud/crudoperation/Service/DepartmentServiceImp.java
@@ -1,7 +1,17 @@
 package com.example.crud.crudoperation.Service;
 
+import com.example.crud.crudoperation.entity.Department;
+import com.example.crud.crudoperation.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DepartmentServiceImp implements DepartmentService{
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public Department saveDepartment(Department department) {
+        departmentRepository.save(department);
+    }
 }
