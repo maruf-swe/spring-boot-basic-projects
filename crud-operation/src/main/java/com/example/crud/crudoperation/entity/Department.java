07f05014368de49c9d7ca828a5c7d8dcd44bcd12
Maruf <marufur1847@gmail.com>
2025-02-22 23:43:31 +0600

    lombok implement

--- crud-operation/src/main/java/com/example/crud/crudoperation/entity/Department.java
+++ crud-operation/src/main/java/com/example/crud/crudoperation/entity/Department.java
@@ -1,6 +1,22 @@
 package com.example.crud.crudoperation.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data  // if u want to add getter setter tostring etc method then @Data annotation is ok
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
     private String departmentName;
     private String departmentAddress;
