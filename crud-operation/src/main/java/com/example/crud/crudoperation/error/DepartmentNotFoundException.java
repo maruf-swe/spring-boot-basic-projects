b7d145640cda25fd910166ae5c838c44ac05835f
Maruf <marufur1847@gmail.com>
2025-02-24 23:56:45 +0600

    exception handling

--- crud-operation/src/main/java/com/example/crud/crudoperation/error/DepartmentNotFoundException.java
+++ crud-operation/src/main/java/com/example/crud/crudoperation/error/DepartmentNotFoundException.java
@@ -2,5 +2,24 @@ package com.example.crud.crudoperation.error;
 
 public class DepartmentNotFoundException extends Exception{
 
-    
+
+    public DepartmentNotFoundException() {
+        super();
+    }
+
+    public DepartmentNotFoundException(String message) {
+        super(message);
+    }
+
+    public DepartmentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DepartmentNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    protected DepartmentNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
