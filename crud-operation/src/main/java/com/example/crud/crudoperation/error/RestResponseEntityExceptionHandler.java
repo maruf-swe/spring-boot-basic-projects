f71fbc9a7e53161c98d4b674aa7ebe285b9e899f
Maruf <marufur1847@gmail.com>
2025-03-03 00:00:13 +0600

    rest respons exception

--- crud-operation/src/main/java/com/example/crud/crudoperation/error/RestResponseEntityExceptionHandler.java
+++ crud-operation/src/main/java/com/example/crud/crudoperation/error/RestResponseEntityExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.example.crud.crudoperation.error;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
+@ResponseStatus
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 }
